872faadcfa112d671b919b344dad3292bcd149a5
Leo Duran <leo-duran@users.noreply.github.com>
2025-06-12 16:13:00 -0600

    Move the catalog dashboard to the components folder

--- src/Plugins/Catalog.Plugin/Client/src/bundle.manifests.ts
+++ src/Plugins/Catalog.Plugin/Client/src/bundle.manifests.ts
@@ -1,5 +1,5 @@
 import { manifests as entrypoints } from "./entrypoints/manifest.js";
-import { manifests as dashboards } from "./components/manifest.js";
+import { manifests as dashboards } from "./components/catalog-dashboard/manifest.js";
 import { manifests as propertyEditors } from "./components/simple-property-editor/manifest.js";
 
 // Job of the bundle is to collate all the manifests from different parts of the extension and load other manifests
