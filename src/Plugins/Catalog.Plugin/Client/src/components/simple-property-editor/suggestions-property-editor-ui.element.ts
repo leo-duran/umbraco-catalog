82ca41a4d7604b24a8e03e222bd42ead82d3f222
Leo Duran <leo-duran@users.noreply.github.com>
2025-06-12 16:10:31 -0600

    Finish the suggestion property editor extension

--- src/Plugins/Catalog.Plugin/Client/src/components/simple-property-editor/suggestions-property-editor-ui.element.ts
+++ src/Plugins/Catalog.Plugin/Client/src/components/simple-property-editor/suggestions-property-editor-ui.element.ts
@@ -1,61 +1,76 @@
-import { LitElement, html, css, customElement, property } from '@umbraco-cms/backoffice/external/lit';
+import { UmbChangeEvent } from '@umbraco-cms/backoffice/event';
+import { css, customElement, html, LitElement, property, state } from '@umbraco-cms/backoffice/external/lit';
 import type { UmbPropertyEditorUiElement } from '@umbraco-cms/backoffice/property-editor';
 import { UmbTextStyles } from '@umbraco-cms/backoffice/style';
 
 @customElement('my-suggestions-property-editor-ui')
 export default class MySuggestionsPropertyEditorUIElement extends LitElement implements UmbPropertyEditorUiElement {
-    @property({ type: String })
-    public value = '';
+	@property({ type: String })
+	public value = '';
 
-    override render() {
-        return html`
-          <uui-input
-            id="suggestion-input"
-            class="element"
-            label="text input"
-            .value=${this.value || ""}
-          >
-          </uui-input>
-          <div id="wrapper">
-            <uui-button
-              id="suggestion-button"
-              class="element"
-              look="primary"
-              label="give me suggestions"
-            >
-              Give me suggestions!
-            </uui-button>
-            <uui-button
-              id="suggestion-trimmer"
-              class="element"
-              look="outline"
-              label="Trim text"
-            >
-              Trim text
-            </uui-button>
-          </div>
-        `;
-    }
+	@state()
+	private _suggestions = [
+		'You should take a break',
+		'I suggest that you visit the Eiffel Tower',
+		'How about starting a book club today or this week?',
+		'Are you hungry?',
+	];
 
-    static override readonly styles = [
-        UmbTextStyles,
-        css`
-          #wrapper {
-            margin-top: 10px;
-            display: flex;
-            gap: 10px;
-          }
-          .element {
-            width: 100%;
-          }
-        `,
-      ];
+	#onInput(e: InputEvent) {
+		this.value = (e.target as HTMLInputElement).value;
+		this.#dispatchChangeEvent();
+	}
 
+	#onSuggestion() {
+		const randomIndex = (this._suggestions.length * Math.random()) | 0;
+		this.value = this._suggestions[randomIndex];
+		this.#dispatchChangeEvent();
+	}
+
+	#dispatchChangeEvent() {
+		this.dispatchEvent(new UmbChangeEvent());
+	}
+
+	override render() {
+		return html`
+			<uui-input
+				id="suggestion-input"
+				class="element"
+				label="text input"
+				.value=${this.value || ''}
+				@input=${this.#onInput}>
+			</uui-input>
+			<div id="wrapper">
+				<uui-button
+					id="suggestion-button"
+					class="element"
+					look="primary"
+					label="give me suggestions"
+					@click=${this.#onSuggestion}>
+					Give me suggestions!
+				</uui-button>
+				<uui-button id="suggestion-trimmer" class="element" look="outline" label="Trim text"> Trim text </uui-button>
+			</div>
+		`;
+	}
+
+	static override readonly styles = [
+		UmbTextStyles,
+		css`
+			#wrapper {
+				margin-top: 10px;
+				display: flex;
+				gap: 10px;
+			}
+			.element {
+				width: 100%;
+			}
+		`,
+	];
 }
 
-
 declare global {
-    interface HTMLElementTagNameMap {
-        'my-suggestions-property-editor-ui': MySuggestionsPropertyEditorUIElement;
-    }
+	interface HTMLElementTagNameMap {
+		'my-suggestions-property-editor-ui': MySuggestionsPropertyEditorUIElement;
+	}
 }
